5039e06df86a30c55202972b56aecc9877e495e7
Hp <Hp@DESKTOP-UCJ09F8.homenet.telecomitalia.it>
2023-11-14 11:14:21 +0100

    update 1

--- src/main/java/it/polito/tdp/RyderCupSimulator/dao/RyderCupDAO.java
+++ src/main/java/it/polito/tdp/RyderCupSimulator/dao/RyderCupDAO.java
@@ -11,7 +11,7 @@ import it.polito.tdp.RyderCupSimulator.model.Player;
 
 public class RyderCupDAO {
 
-	/*public List<Player> getAllPlayers(){
+	public List<Player> getAllPlayers(){
 		String query = "SELECT DISTINCT * "
 				+ "FROM ranking r ";
 		List<Player> result = new ArrayList<Player>();
@@ -21,12 +21,25 @@ public class RyderCupDAO {
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
-				String nome = rs.getString("First Name");
-				String cognome; 
-				String nazione;
-				Integer appearances; 
-				Integer posizioneRanking;
-				Integer totaleIncassiAnno;
+				String nomeS = rs.getString("First Name");
+				String nome = nomeS.substring(1, nomeS.length()-1);//devo togliere virgolette dai valori del db!!!
+				
+				String cognomeS = rs.getString("Last Name"); 
+				String cognome = cognomeS.substring(1, cognomeS.length()-1);
+				
+				String nazioneS = rs.getString("CTRY");
+				String nazione = nazioneS.substring(1, nazioneS.length()-1);
+				
+				String nAppS = rs.getString("EVENTS PLAYED (ACTUAL)");
+				String nApp = nAppS.substring(1, nAppS.length()-1);
+				Integer appearances = Integer.parseInt(nApp); 
+				
+				
+				String posizioneRankingS = rs.getString("RankPosition");
+				String pos = posizioneRankingS.substring(1, posizioneRankingS.length()-1);
+				Integer posizioneRanking = Integer.parseInt(pos); 
+				
+				Integer totaleIncassiAnno = 0;//per calcolo incassi posso usare come chiave primaria nomecognomenazionalita
 				Player p = new Player(nome, cognome, nazione, appearances, posizioneRanking, totaleIncassiAnno);
 				result.add(p);
 			}
@@ -37,7 +50,8 @@ public class RyderCupDAO {
 			System.out.println("Errore connessione al database");
 			throw new RuntimeException("Error Connection Database");
 		}
-	}*/
+	}
 		
+	
 	
 }
--- src/main/java/it/polito/tdp/RyderCupSimulator/model/Model.java
+++ src/main/java/it/polito/tdp/RyderCupSimulator/model/Model.java
@@ -1,6 +1,35 @@
 package it.polito.tdp.RyderCupSimulator.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import it.polito.tdp.RyderCupSimulator.dao.RyderCupDAO;
+
 public class Model {
 	
+	private RyderCupDAO dao;
+	private List<Player>players;
+	private Map<String,Player>idMapPlayers;
+	private List<Player>teamEUR;//questo sarà il team Europeo selezionato
+	private List<Player>teamUSA;//questo sarà il team USA selezionato
+
 	
+	public Model() {
+		this.dao = new RyderCupDAO();
+		this.teamEUR = new ArrayList<>();
+		this.teamUSA = new ArrayList<>();
+		this.idMapPlayers = new HashMap<>();
+	}
+	
+	public void loadPlayers() {
+		this.players = new ArrayList<>(dao.getAllPlayers());
+	}
+	
+	/*public List<Player>selezionaTeam(Integer nAppMin, List<Player>disponibili){//la lista disponibili contiene tutti i playerUSA se sto facendo il teamUSA, tutti quelli EURO se sto facendo team EURO
+		
+		return ;
+		
+	}*/
 }
